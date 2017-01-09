4895727a0661e28aefa6325fbd247fce598f16d9
Jian Wu <xcb479@alumni.ku.dk>
2017-01-09 14:21:52 +0100

    Add files via upload

--- CodeBook_generation.py
+++ CodeBook_generation.py
@@ -1,7 +1,10 @@
+from numpy import *
 import cv2
+import os
 from scipy.cluster.vq import *
 
-class Vocabuary(object):
+
+class Vocabulary(object):
     def __init__(self,name):
         self.name = name
         self.voc = []
